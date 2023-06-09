e8d8ebb3ce65218ba129bb5569ee0648fce6e1e2
clara.schmukler@gmail.com <clara.schmukler@gmail.com>
2023-06-09 00:51:31 -0300

    script contenedor clickeable

--- JS/script.js
+++ JS/script.js
@@ -31,11 +31,114 @@ var mysong3 =new Audio();
 mysong3.src="./audio/domino wayv.mp3";
 const playsong3=()=>{
     mysong3.play();
- 
 }
+
 const pausesong3=()=>{
     mysong3.pause();
 }
+
+var mysong4 =new Audio();
+mysong4.src="./audio/peek a boo red velvet.mp3";
+const playsong4=()=>{
+    mysong4.play();
+}
+const pausesong4=()=>{
+    mysong4.pause();
+}
+
+// AUDIO 5
+var mysong5 =new Audio();
+mysong5.src="./audio/power exo.mp3";
+const playsong5=()=>{
+    mysong5.play();
+ 
+}
+const pausesong5=()=>{
+    mysong5.pause();
+}
+
+var mysong6 =new Audio();
+mysong6.src="./audio/x fx.mp3";
+const playsong6=()=>{
+    mysong6.play();
+ 
+}
+const pausesong6=()=>{
+    mysong6.pause();
+}
+
+
+var mysong7 =new Audio();
+mysong7.src="./audio/view shinee .mp3";
+const playsong7=()=>{
+    mysong7.play();
+ 
+}
+const pausesong7=()=>{
+    mysong7.pause();
+}
+
+
+
+
+
+
+
+var mysong8 =new Audio();
+mysong8.src="./audio/into snsd.mp3";
+const playsong8=()=>{
+    mysong8.play();
+ 
+}
+const pausesong8=()=>{
+    mysong8.pause();
+}
+
+
+var mysong9 =new Audio();
+mysong9.src="./audio/black suit suju.mp3";
+const playsong9=()=>{
+    mysong9.play();
+ 
+}
+const pausesong9=()=>{
+    mysong9.pause();
+}
+
+
+var mysong10 =new Audio();
+mysong10.src="./audio/catch me tvxq.mp3";
+const playsong10=()=>{
+    mysong10.play();
+ 
+}
+const pausesong10=()=>{
+    mysong10.pause();
+}
+
+
+var mysong11 =new Audio();
+mysong11.src="./audio/only one boa.mp3";
+const playsong11=()=>{
+    mysong11.play();
+ 
+}
+const pausesong11=()=>{
+    mysong11.pause();
+}
+
+
+var mysong12 =new Audio();
+mysong12.src="./audio/polaris kangta.mp3";
+const playsong12=()=>{
+    mysong12.play();
+ 
+}
+const pausesong12=()=>{
+    mysong12.pause();
+}
+
+
 
 
 
